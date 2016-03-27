d6a37fcd6407ed8771cd3f18d9ed87ad60fad5ff
auliude <eric@ericliu.io>
2016-03-27 19:19:45 +0000

    Remove env

--- blog/config.py
+++ blog/config.py
@@ -3,3 +3,8 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
+    
+class TravisConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
+    DEBUG = False
+    SECRET_KEY = "Not secret"
